947982a628a431b1f35fd4b425d368e4ad5809b6
ralph <remmason@yahoo.com>
2024-07-15 10:17:10 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
 
+
+
 const App = () => {
   return (
     <>
-    
     {/* <h1>Testimonies </h1>  */}
-    <img src="images/modi02.png" alt="Example"  className="example-image" />
+    <img src="images/rit001.png" alt="Example" 
+     className="example-image" />
+     <nav className="bg-indigo-700 border-b border-indigo-500">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <div
+            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <div className="md:ml-auto">
+            <a 
+              href="https://latest-pavk.onrender.com/"
+                 className="text-white bg-grey hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                   Continue Reading -
+         </a>
+              <div className="flex space-x-2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-black text-white p-6 rounded-lg shadow-md">
+          <div className="bg-black text-white p-6 rounded-lg shadow-md"> 
             <h1 className="text-2xl font-bold">Frontend Courses</h1>
             <p></p>
             <p className="text-2xl mb-4">
@@ -26,8 +46,7 @@ const App = () => {
             </p>
             <a 
             href="https://latest-pavk.onrender.com/"
-              className="inline-block bg-indigo text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
+              className="inline-block bg-indigo text-white rounded-lg px-4 py-2 hover:bg-gray-700">
               Read More
             </a>
           </div>
@@ -39,7 +58,7 @@ const App = () => {
             HackerRank
             </p>
             <p className="text-2xl mb-4">
-            GeekforGeek
+            GeeksforGeeks
             </p>
             <p className="text-2xl mb-4">
             Codility
@@ -47,8 +66,7 @@ const App = () => {
             <p className="text-2xl mb-4">
             Codify
             </p>
-            
-            <a 
+        <a 
             href="https://latest-pavk.onrender.com/projects"
               className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
@@ -66,7 +84,16 @@ const App = () => {
             </h2>
           </header>
         </div>
-    </>
+
+        <div className="footer-below">
+        <div className="footer-copyright">
+          <p>
+              {(new Date().getFullYear())} All right reserved ---&gt; (Rittechnology and Training). 
+          </p>
+        </div>
+        </div>
+  </>
+        
   )
 }
 
